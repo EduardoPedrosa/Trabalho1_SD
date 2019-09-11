6fec751dea1671dae2d524625d38b95ecd200a81
Eduardo Pedrosa <dudupedrosao@gmail.com>
2019-09-11 18:33:35 -0300

    commit inicial

--- clientTCP.py
+++ clientTCP.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
 tcp_ip = '127.0.0.1'
 tcp_port = 5006
@@ -9,11 +10,10 @@ s = socket.socket(socket.AF_INET,
 				socket.SOCK_STREAM)
 s.connect((tcp_ip, tcp_port))
 
-while True:
-	message = raw_input("Digite Sua Mensagem: ")
-	s.send(message)
-
-	data = s.recv(1024) #Tamanho do buffer.
-	print "Mensagem recebida: ", data
+begin = time.time()
+s.send("a".encode())
+data = s.recv(1024) #Tamanho do buffer.
+end = time.time()
+print ("Tempo gasto: ", (end-begin)/1000000)
 
 s.close()
