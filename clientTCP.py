4fa26196d5475c14d2f09634ea053ab0da655c64
Eduardo Pedrosa <dudupedrosao@gmail.com>
2019-09-11 21:30:22 -0300

    cliente RTT

--- clientTCP.py
+++ clientTCP.py
@@ -7,13 +7,30 @@ tcp_ip = '127.0.0.1'
 tcp_port = 5006
 
 s = socket.socket(socket.AF_INET,
-				socket.SOCK_STREAM)
+				socket.SOCK_DGRAM)
 s.connect((tcp_ip, tcp_port))
 
-begin = time.time()
-s.send("a".encode())
-data = s.recv(1024) #Tamanho do buffer.
-end = time.time()
-print ("Tempo gasto: ", (end-begin)/1000000)
+times = []
+total = 0
+quantity = 20
+
+for x in quantity:
+	begin = time.time()
+	s.send("a".encode())
+	data = s.recv(1024) #Tamanho do buffer.
+	end = time.time()
+	parcialTime = (end-begin)
+	print ("Tempo gasto: ", parcialTime/1000000, data)
+	times[x] = data
+	total += parcialTime
+	time.sleep(0.5)
+
+mean = total/quantity
+print ("Média: ", mean)
+result = 0
+for x in quantity:
+	result += ((times[x] - mean) ** 2)
+deviation = ((result) ** (1/2))
+print ("Desvio padrão: ", deviation)
 
 s.close()
